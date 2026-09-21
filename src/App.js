821ccbfe52badb45baca9e6d4698ae7989bccd61
mobiworld3212-creator <mobiworld3212@gmail.com>
2026-09-21 15:30:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <>
+      <Hero />
+
+      {/* Your other sections */}
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-8 text-center">
+      <p>© 2026 E-Commerce. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
